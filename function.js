08376968f8ac7bce7e676d4731f3be119fd7ecba
Peal1997 <hasanpeal65@gmail.com>
2022-12-14 18:34:05 -0600

    first save

---
+++ function.js
@@ -0,0 +1,4 @@
+const alert = (msg , type) =>{
+    return `<span class = "alert alert-${type} d-flex justify-content-between">${msg}<button class = "btn-close" data-bs-dismiss = "alert"></button></span>`
+}
+
